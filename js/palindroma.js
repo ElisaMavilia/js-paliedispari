723a0a44fe3aeea33372af370ace720470f4ab4c
ElisaMavilia <elisamavilia1@gmail.com>
2024-03-14 15:53:19 +0100

    add result

--- js/palindroma.js
+++ js/palindroma.js
@@ -2,7 +2,7 @@
 Chiedere all’utente di inserire una parola 
 Creare una funzione per capire se la parola inserita è palindroma */
 
-let userWord = prompt("Inserisci una parola");
+/* let userWord = prompt("Inserisci una parola");
 console.log(userWord);
 
 function checkPalindrome (palindrome){
@@ -26,6 +26,6 @@ if(userWord === reverseWord) {
  } else{
     console.log('La parola non è palindroma');
  }
-
+ */
 
 
---
+++ js/paridispari.js
@@ -0,0 +1,34 @@
+/* Pari e Dispari 
+L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. 
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). 
+Sommiamo i due numeri 
+Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) 
+Dichiariamo chi ha vinto. */
+
+let userChoice = prompt("Scegli tra pari o dispari");
+let userNumber = parseInt(prompt("Inserisci un numero tra 1 e 5"));
+console.log(userChoice, userNumber);
+
+let pcNumber = getRndInteger(1,10);
+console.log(pcNumber);
+
+let numbersSum = (userNumber + pcNumber);
+console.log(numbersSum);
+
+function result(odd, even){
+    if(numbersSum % 2 ===0){
+      return(even);
+    } else{
+        return(odd);
+    }
+}
+
+if(userChoice === result){
+    console.log("Hai vinto")
+    } else{
+        console.log("Hai perso");
+    }
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
